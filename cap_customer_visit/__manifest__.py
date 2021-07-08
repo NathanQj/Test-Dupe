d3676d46cf0fa3989c5665e4767d2bbc7764f763
NathanQ <nathan.qjm@gmail.com>
2021-07-08 04:38:20 -0400

    add cap modules

---
+++ cap_customer_visit/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Partner Modification",
+    'summary': """
+        This module modifies the partners' model to add an latest visit date field.
+        """,
+    'description': """ """,
+    'category': 'CRM',
+    'version': '12.0.1.0.1',
+    'depends': ['base'],
+    'data': [
+        'views/res_partner.xml',
+    ],
+}
---
+++ cap_website_calendar/models/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 11.
+
+from . import calendar_appointment_type
+from . import res_partner
